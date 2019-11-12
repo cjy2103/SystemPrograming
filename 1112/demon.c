2263c6841f35ebaf1121d91edd93672d6042cd06
cjy2103 <43211800+cjy2103@users.noreply.github.com>
2019-11-12 11:21:06 +0900

    Add files via upload

---
+++ 1112/demon.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<unistd.h>
+int main(){
+	pid_t pid;
+	if((pid = fork()) < 0)
+		exit(0);
+	else if (pid != 0 )
+		exit(0);
+	signal(SIGHUP,SIG_IGN);
+	clise(0);
+	close(1);
+	close(2);
+
+	setsid();
+	while(1)
+		printf("Hello C world!");
+		sleep(1);
+	}
+
